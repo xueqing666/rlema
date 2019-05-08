4a21ff3c28b4d32b75c79fe90335844e7416b0e0
xxl335903487 <335903487@qq.com>
2019-05-08 21:10:37 +0800

    xxl--商品页面

---
+++ src/classification/hot/hot.vue
@@ -0,0 +1,184 @@
+<template>
+  <div>
+    <div class="left">
+      <ul>
+        <li>
+          <div>热销榜</div>
+        </li>
+        <li>
+          <div><img src="../../img/special_3@2x.png" alt=""><span>单人特色套餐</span></div>
+        </li>
+        <li>
+          <div>特色粥品</div>
+        </li>
+        <li>
+          <div>精选热菜</div>
+        </li>
+        <li>
+          <div>爽口凉菜</div>
+        </li>
+        <li>
+          <div>半成品</div>
+        </li>
+        <li>
+          <div>饭类</div>
+        </li>
+        <li>
+          <div>面类</div>
+        </li>
+      </ul>
+    </div>
+    <div class="right">
+      <div class="top">单人特色套餐</div>
+      <div class="content">
+        <div class="every">
+          <div class="every_left">
+            <img src="../../img/invoice_1@3x.png" alt="">
+          </div>
+          <div class="every_middle">
+            <div class="every_middle_top">皮蛋瘦肉粥配包子套餐</div>
+            <div class="every_middle_button">月售1132份 好评率100%</div>
+            <div class="every_middle_foot">￥24<span>￥28</span></div>
+          </div>
+          <div class="every_right">
+            <span><img src="../../img/remove_circle_outline.svg" alt=""></span>
+            <span class="singletonNumber">1</span>
+            <span><img src="../../img/add_circle.svg" alt=""></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "hot"
+  }
+</script>
+
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  .left {
+    width: 160px;
+    font-weight: 200;
+    float: left;
+    margin-top: 2px;
+  }
+
+  ul > li:first-child {
+    padding-top: 32px;
+  }
+
+  ul > li {
+    width: 100%;
+    height: 108px;
+    font-size: 22px;
+    list-style: none;
+    background-color: #f3f5f7;
+
+  }
+
+  li > div {
+    width: 112px;
+    margin: 0 auto;
+    height: 84px;
+  }
+
+  li > div > img {
+    width: 25px;
+    height: 25px;
+    vertical-align: top;
+  }
+
+  .right {
+    width: 565px;
+    float: left;
+  }
+
+  .top {
+    width: 102%;
+    height: 52px;
+    font-size: 24px;
+    color: rgb(147, 153, 159);
+    line-height: 52px;
+    background-color: #f3f5f7;
+    border-left: 5px solid #d9dde1;
+    text-align: left;
+    text-indent: 2em;
+  }
+
+  .content {
+    width: 493px;
+    margin: 36px auto;
+    border-bottom: 1px solid gainsboro;
+
+  }
+
+  .every {
+    overflow: hidden;
+  }
+
+  .every_left {
+    float: left;
+  }
+
+  .every_left img {
+    width: 100px;
+    height: 100px;
+  }
+
+  .every_middle {
+    float: left;
+  }
+
+  .every_middle_top {
+    margin-top: 20px;
+    font-size: 28px;
+    color: rgb(7, 17, 27);
+    line-height: 28px;
+    font-weight: 700;
+  }
+
+  .every_middle_button {
+    font-size: 20px;
+    line-height: 50px;
+    color: rgb(147, 153, 159);
+    text-align: left;
+  }
+
+  .every_middle_foot {
+    font-size: 28px;
+    color: rgb(240, 20, 20);
+    font-weight: 700;
+    line-height: 48px;
+    text-align: left;
+  }
+
+  .every_middle_foot > span {
+    font-size: 20px;
+    color: rgb(147, 153, 159);
+    font-weight: 700;
+    line-height: 48px;
+    text-decoration: line-through;
+    margin-left: 16px;
+  }
+
+  .every_right {
+    width: 110px;
+    float: left;
+    margin-top: 100px;
+    font-size: 26px;
+    color: rgb(147, 153, 159);
+  }
+
+  .every_right span img {
+    width: 40px;
+    height: 40px;
+    vertical-align: middle;
+  }
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,59 @@
 <template>
-  <div class="hello">
-    <h1>这是饿了吗主页面</h1>
-    <h2>{{msg}}</h2>
+  <div>
+    <mt-header title="饿了么">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+
+    <mt-navbar v-model="selected">
+      <mt-tab-item id="1">商品</mt-tab-item>
+      <mt-tab-item id="2">评价</mt-tab-item>
+      <mt-tab-item id="3">商家</mt-tab-item>
+    </mt-navbar>
+
+    <!--tab-container -->
+    <mt-tab-container v-model="selected">
+      <mt-tab-container-item id="1">
+        <!--<mt-cell v-for="n in 10" :key="n" :title="'内容 ' + n" />-->
+        <hot/>
+      </mt-tab-container-item>
+    </mt-tab-container>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'hello'
+  import hot from "../classification/hot/hot"
+  export default {
+    name: 'HelloWorld',
+    components:{
+      hot
+    },
+    data() {
+      return {
+        active: '1',
+        selected: ''
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  /*h1, h2 {*/
+    /*font-weight: normal;*/
+  /*}*/
+
+  /*ul {*/
+    /*list-style-type: none;*/
+    /*padding: 0;*/
+  /*}*/
+
+  /*li {*/
+    /*display: inline-block;*/
+    /*margin: 0 10px;*/
+  /*}*/
+
 </style>
