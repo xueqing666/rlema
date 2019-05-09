e2b6c7934c6f19b50caa5ff7fb4b69e2535dfffe
823226328 <823226328@qq.com>
2019-05-09 09:45:33 +0800

    ll--评价页--样式

---
+++ src/components/Headers.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="head">
+      <div class="main">
+        <div class="left">
+          <img :src="avatar" style="width: 128px;height: 128px;border-radius: 4px">
+        </div>
+        <div class="right">
+<!--          <img src="" alt="">-->
+        </div>
+        
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Headers",
+      data(){
+          return{
+            avatar:''
+          }
+      },
+      created() {
+          var _this = this
+          this.$axios.get("/api/sell/shop/info?shopId=1544670774658295899")
+            .then(function (res) {
+              _this.avatar = res.data.data.avatar
+            })
+      }
+    }
+</script>
+
+<style scoped>
+.head{
+  color: rgba(7,17,27,.5);
+  blur:10px;
+  padding-top: 48px;
+  padding-left: 48px;
+}
+</style>
